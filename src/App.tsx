ff0f06df0e5b68b30c8c799ace11a7a61bce78b2
Kawauchi Yuhei <kawauchiyuhei@KawauchinoMacBook-Pro.local>
2024-08-03 11:21:47 +0900

    initial app

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+export const App = () => {
+  return (
+    <div>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <input 
+          type="text" 
+          value="" 
+          onChange={(e) => e.preventDefault()} />
+        <input
+          type="submit"
+          value="追加"
+          onSubmit={(e) => e.preventDefault()}
+        />
+      </form>
+    </div>
+  );
+};
