c02a59115b97f043a428738777e6364b8d144309
Kawauchi Yuhei <kawauchiyuhei@KawauchinoMacBook-Pro.local>
2024-08-03 17:47:05 +0900

    LocalForage以外完成

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,146 @@
+// React から useState フックをインポート
+import { useState } from 'react';
+
+// "Todo型" の定義
+type Todo = {
+  value : string;
+  readonly id: number;
+  checked: boolean;
+  removed: boolean;
+}
+
+type Filter = 'all' | 'checked' | 'unchecked' | 'removed';
+
 export const App = () => {
+  // 初期値: 空文字列 ''
+  const [text, setText] = useState('');
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [filter, setFilter] = useState<Filter>('all');
+
+  const filteredTodos = todos.filter((todo) => {
+    switch (filter) {
+      case 'all':
+        return !todo.removed;
+      case 'checked':
+        return todo.checked && !todo.removed;
+      case 'unchecked':
+        return !todo.checked && !todo.removed;
+      case 'removed':
+        return todo.removed;
+      default:
+        return todo;
+    }
+  })
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+  }
+
+  const handleSubmit = () => {
+    if (!text) return;
+
+    const newTodo: Todo = {
+      value: text,
+      id: new Date().getTime(),
+      checked: false,
+      removed: false,
+    };
+
+    /**
+     * 更新前の todos ステートを元に
+     * スプレッド構文で展開した要素へ
+     * newTodo を加えた新しい配列でステートを更新
+    **/
+    setTodos((todos) => [newTodo, ...todos]);
+    
+    // フォームへの入力をクリアする
+    setText('');
+  };
+
+  const handleTodo = <K extends keyof Todo, V extends Todo[K]>(
+    id: number,
+    key: K,
+    value: V
+  ) => {
+    setTodos((todos) => {
+      const newTodos = todos.map((todo) => {
+        if(todo.id === id){
+          return { ...todo, [key]: value };
+        }else{
+          return todo;
+        }
+      });
+      return newTodos;
+    });
+  };
+
+  const handleFilter = (filter: Filter) => {
+    setFilter(filter);
+  };
+
+  const handleEmpty = () => {
+    // シャローコピーで事足りる。
+    setTodos((todos) => todos.filter((todo) => !todo.removed));
+  }
+
   return (
     <div>
-      <form onSubmit={(e) => e.preventDefault()}>
-        <input 
-          type="text" 
-          value="" 
-          onChange={(e) => e.preventDefault()} />
-        <input
-          type="submit"
-          value="追加"
-          onSubmit={(e) => e.preventDefault()}
-        />
-      </form>
+      <select defaultValue= "all" onChange={(e) => handleFilter(e.target.value as Filter)}>
+        <option value='all'>すべてのタスク</option>
+        <option value='checked'>完了したタスク</option>
+        <option value='unchecked'>現在のタスク</option>
+        <option value='removed'>ゴミ箱</option>
+      </select>
+
+      {filter === 'removed' ? (
+        <button
+          onClick={handleEmpty}
+          disabled={todos.filter((todo) => todo.removed).length === 0}
+        >
+          ゴミ箱を空にする
+        </button>
+      ) : (
+        filter !== 'checked' && (
+          <form onSubmit={(e) => {
+            e.preventDefault();
+            handleSubmit();
+          }}
+          >
+            <input 
+              type="text" 
+              value={text}
+              onChange={(e) => handleChange(e)} />
+            <input
+              type="submit"
+              value="追加"
+              onSubmit={handleSubmit}
+            />
+          </form>
+        )
+      )}
+      
+      <ul>
+        {filteredTodos.map((todo) => {
+          return (
+            <li key={todo.id}>
+              <input
+                type = "checkbox"
+                disabled = {todo.removed}
+                checked = {todo.checked}
+                onChange={() => handleTodo(todo.id, 'checked', !todo.checked)}
+              />
+              <input
+                type="text"
+                disabled = {todo.checked || todo.removed}
+                value={todo.value}
+                onChange={(e) => handleTodo(todo.id, 'value', e.target.value)}
+              />
+              <button onClick={() => handleTodo(todo.id, 'removed', !todo.removed)}>
+                {todo.removed ? '復元' : '削除'}</button>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
